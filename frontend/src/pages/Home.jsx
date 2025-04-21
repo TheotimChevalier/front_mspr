04543bb84bba6bfadab1426c98d65706567af1d5
Polak-Man <logan.vourey@ecoles-epsi.net>
2025-04-21 17:39:05 +0200

    ajout Titre

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,11 @@
 // src/pages/Home.jsx
+import React, { useEffect } from 'react';
 import DiabetesPredictor from "../components/DiabetesPredictor";
 
 export default function Home() {
+  useEffect(() => {
+    document.title = "Prediction Diabete"; // Changez le titre ici
+  }, []); // Le tableau vide signifie que cela ne s'exécute qu'une fois, lors du premier rendu
+
   return <DiabetesPredictor />;
 }
